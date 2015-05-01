a9f4d80eb890769255310637d386abb530eeedb7
Matthias Benkort <matthias.benkort@gmail.com>
2015-05-01 18:07:26 +0200

    Create a test branch.
    Now, 'init' can init a git repository and create a 'test' branch.
    The branch is populated with some files that consitutea test env for the lib/widget.
    Module are processed yet.

---
+++ task_runner/widget/gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp'),
+    exec = require('child_process').exec,
+    path = require('path'),
+    fs = require('fs');
+
+gulp.task('test', function (done) {
+    var testDir = path.join(process.cwd(), 'test');
+        files = fs.readdirSync(process.cwd()).filter(function (el) {
+            return el.match(/^(test|\.git|gulpfile.js|\.travis\.yml|node_modules)$/) === null;
+        }),
+        projectDir = path.join(testDir, fs.readdirSync(testDir).pop()),
+        widgetDir = path.join(projectDir, 'app', 'widget');
+
+    exec('cp -r ' + files.join(" ") + " " + widgetDir, function (err) {
+        if (err) { process.exit(1); }
+        
+        process.chdir(projectDir);
+        exec('gulp test:jasmine', done).stdout.pipe(process.stdout);
+    });
+});
